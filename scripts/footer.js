51d94757e2ac8cb61cca200f208c61030f6b074f
chika-code <119892340+nzoh-nkumeAjang@users.noreply.github.com>
2023-05-02 13:15:27 +0200

    illuminati

---
+++ scripts/footer.js
@@ -0,0 +1,13 @@
+const createFooter = () => {
+    let footer = document.querySelector('footer')
+
+    footer.innerHTML = `
+    <div class="copyright">
+        <p>&copy; copyright 2022  <a href="#">  secrete society - the light, the eye, the pyramid</a>. All Rights Reserved.</p>
+    </div>
+    <div class="copyright">
+        <p>Designed by <a href="#">ILLUMINATI_OFFICIAL</a></p>
+    </div>
+    `;
+}
+createFooter();
